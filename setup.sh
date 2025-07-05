5abca91ecb8df62d22b6b8e6ac37d35fb660e2c2
ameen-abdallah <ameen.abdallah@hotmail.com>
2025-07-05 12:05:06 +0200

    Fixing script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ sudo pacman -S git ansible curl
 
 ansible-galaxy collection install kewlfft.aur
 
-curl -s https://github.com/ameen-abdallah/desktop-configs/setup.sh | bash
-
 ansible-pull --ask-become-pass -U https://github.com/ameen-abdallah/desktop-configs
 
 echo "Ansible installation complete."
