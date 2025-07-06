50e4700cf1840ebfcb60ecf96932f9aa6d50598a
ameen-abdallah <ameen.abdallah@hotmail.com>
2025-07-06 18:10:11 +0200

    Updating the scipt

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ sudo pacman -S git ansible curl
 
 ansible-galaxy collection install kewlfft.aur
 
-ansible-pull --ask-become-pass -U https://github.com/ameen-abdallah/desktop-configs
+ansible-pull --ask-become-pass -e "ansible_distribution=Archlinux" -U https://github.com/ameen-abdallah/desktop-configs --clean -d desktop-configs/
 
 echo "Ansible installation complete."
