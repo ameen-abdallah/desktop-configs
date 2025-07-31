6fd328f54d3969dd808ab653fd2d89fa4808be60
Ameen Abdallah <ameen.abdallah@hotmail.com>
2025-07-31 19:34:29 +0200

    more fixes

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ sudo pacman -S git ansible curl
 
 ansible-galaxy collection install kewlfft.aur
 
-ansible-pull --ask-become-pass -e "ansible_distribution=Archlinux" -U https://github.com/ameen-abdallah/desktop-configs --clean -d desktop-configs/
+ansible-pull --ask-become-pass -u ameen--force -i arch-test1, -e "ansible_distribution=Archlinux" -U https://github.com/ameen-abdallah/desktop-configs -clean -d desktop-c>
 
 echo "Ansible installation complete."
