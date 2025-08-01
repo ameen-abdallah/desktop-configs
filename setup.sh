a8bb287c47c7e7da945d1f43abe6c7b1c06b5d09
Ameen Abdallah <ameen.abdallah@hotmail.com>
2025-08-01 16:20:34 +0200

    fixed inventory

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ sudo mkdir -p /var/log/ansible
 sudo chmod -R 755 /var/log/ansible
 sudo chown -R ameen:ameen /var/log/ansible
 
-ansible-pull --ask-become-pass --force -i arch-test1, -e "ansible_distribution=Archlinux" -U https://github.com/ameen-abdallah/desktop-configs -clean -d desktop-configs/
+ansible-pull --ask-become-pass --force -i ameen-linux, -e "ansible_distribution=Archlinux" -U https://github.com/ameen-abdallah/desktop-configs -clean -d desktop-configs/
 
 echo "Ansible installation complete."
